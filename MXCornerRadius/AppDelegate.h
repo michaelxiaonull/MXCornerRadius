156df4f44b364d5abbbd15adb35a6f70fdca2fcb
GaoYouJian <740150254@qq.com>
2018-11-27 22:05:02 +0800

    Initial Commit

---
+++ MXCornerRadius/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MXCornerRadius
+//
+//  Created by Michael on 2018/11/27.
+//  Copyright © 2018 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MXCornerRadius/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MXCornerRadius
+//
+//  Created by Michael on 2018/11/27.
+//  Copyright © 2018 Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
